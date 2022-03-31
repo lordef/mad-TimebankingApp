42d7599f2665afba3063bdbb8ef0fff400a008c8
Andrea Palomba <s291692@studenti.polito.it>
2022-03-31 19:31:38 +0200

    stable activity_edit_profile with division 1/3-2/3

--- app/src/main/java/it/polito/mad/lab02/EditProfileActivity.kt
+++ app/src/main/java/it/polito/mad/lab02/EditProfileActivity.kt
@@ -1,11 +1,52 @@
 package it.polito.mad.lab02
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewTreeObserver
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        /* Divide screen in 1/3 and 2/3 */
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            val firstLayout = findViewById<ConstraintLayout>(R.id.upperConstraintLayout)
+            val father = findViewById<LinearLayout>(R.id.mainLinearLayout)
+
+            father.viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    val h = father.height
+                    val w = father.width
+                    Log.d("Layout", "firstLayout.requestLayout(): $w,$h")
+                    firstLayout.post {
+                        firstLayout.layoutParams = LinearLayout.LayoutParams(w/3, h)
+                    }
+                    father.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+        } else {
+            val firstLayout = findViewById<ConstraintLayout>(R.id.upperConstraintLayout)
+            val sv = findViewById<ScrollView>(R.id.mainScrollView)
+
+            sv.viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    val h = sv.height
+                    val w = sv.width
+                    Log.d("Layout", "firstLayout.requestLayout(): $w,$h")
+                    firstLayout.post {
+                        firstLayout.layoutParams = LinearLayout.LayoutParams(w, h / 3)
+                    }
+                    sv.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+        }
     }
 }
