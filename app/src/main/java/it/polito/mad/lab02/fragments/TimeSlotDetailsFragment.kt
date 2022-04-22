28439b03855bc39df99af22982ef6d08e8b6f3d8
Andrea Palomba <s291692@studenti.polito.it>
2022-04-22 12:39:42 +0200

    Point 4 - layout modified

--- app/src/main/java/it/polito/mad/lab02/fragments/TimeSlotDetailsFragment.kt
+++ app/src/main/java/it/polito/mad/lab02/fragments/TimeSlotDetailsFragment.kt
@@ -1,17 +1,25 @@
 package it.polito.mad.lab02.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentTransaction
 import com.google.gson.Gson
+import it.polito.mad.lab02.EditProfileActivity
 import it.polito.mad.lab02.R
 import it.polito.mad.lab02.SharedPreference
 import it.polito.mad.lab02.models.TimeSlotDetailsModel
@@ -86,15 +94,16 @@ class TimeSlotDetailsFragment : Fragment(R.layout.fragment_time_slot_details) {
 
         return when (item.itemId) {
             R.id.editItem -> {
-                Toast.makeText(this.context, "Edit TimeSlotDetails selected", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(this.context, "Edit TimeSlotDetails selected", Toast.LENGTH_SHORT).show()
                 // TODO: manage transition to TimeSlotEditFragment
+
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
 
     }
+
 
 
 }
