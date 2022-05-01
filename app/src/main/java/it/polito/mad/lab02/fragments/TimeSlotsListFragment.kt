252c6e0c7d927c24231efb0c0f80311010ad3c53
Fabiano Campion <fabianocampion97@gmail.com>
2022-05-01 17:27:06 +0200

    Changed TimeSlotListFragment.kt

--- app/src/main/java/it/polito/mad/lab02/fragments/TimeSlotsListFragment.kt
+++ app/src/main/java/it/polito/mad/lab02/fragments/TimeSlotsListFragment.kt
@@ -2,21 +2,21 @@ package it.polito.mad.lab02.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import it.polito.mad.lab02.R
 import it.polito.mad.lab02.viewmodels.TimeSlotListViewModel
 
 /**
  * A fragment representing a list of Items.
  */
-class TimeSlotsListFragment : Fragment() {
+class TimeSlotsListFragment : Fragment(R.layout.fragment_time_slot_list) {
 
     private var columnCount = 1
 
@@ -32,35 +32,30 @@ class TimeSlotsListFragment : Fragment() {
         */
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        val listView = inflater.inflate(R.layout.fragment_time_slot_list, container, false)
-        val recyclerView = listView.findViewById<RecyclerView>(R.id.list)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.list)
+        val timeSlotList = vm.getTimeSlotList().value
         if (recyclerView is RecyclerView) {
             with(recyclerView) {
                 layoutManager = when {
                     columnCount <= 1 -> LinearLayoutManager(context)
                     else -> GridLayoutManager(context, columnCount)
                 }
-                adapter = TimeSlotsListRecyclerViewAdapter(vm.getTimeSlotList().value!!)
+                //TODO: add observer as in details fragment
+                adapter = TimeSlotsListRecyclerViewAdapter(timeSlotList!!)
             }
         }
-        if(vm.getTimeSlotList().value!!.size == 0){
-            recyclerView.visibility = View.GONE
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab2)
+        fab.setOnClickListener { view ->
+            view.findNavController()
+                .navigate(R.id.action_nav_advertisement_to_timeSlotEditFragment)
         }
-        else{
-            val textView = listView.findViewById<TextView>(R.id.text_advertisements)
+        if (timeSlotList!!.size == 0) {
+            recyclerView.visibility = View.GONE
+        } else {
+            val textView = view.findViewById<TextView>(R.id.text_advertisements)
             textView.visibility = View.GONE
         }
-
-        return listView
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 }
