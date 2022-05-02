ba5d109a209c05b4fd5f58fc4038645b9c878505
lordef <lorenzo.defilippis02@gmail.com>
2022-05-02 17:48:02 +0200

    minor changes

--- app/src/main/java/it/polito/mad/lab02/SharedPreference.kt
+++ app/src/main/java/it/polito/mad/lab02/SharedPreference.kt
@@ -67,7 +67,6 @@ class SharedPreference(context: Context) {
                 timeSlotListTemp.add(timeSlotDetails)
             }
         }
-        println(timeSlotListTemp)
         return timeSlotListTemp
     }
 
