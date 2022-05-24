67e18dcc399988a2bad620e87d4f38992fd77b9d
Ferdinando Micco <s290247@studenti.polito.it>
2022-05-24 20:24:51 +0200

    added alertDialogue so that the user add at least one skill before creating a timeslot

--- app/src/main/java/it/polito/mad/lab02/fragments/myadvertisements/TimeSlotsListFragment.kt
+++ app/src/main/java/it/polito/mad/lab02/fragments/myadvertisements/TimeSlotsListFragment.kt
@@ -1,17 +1,22 @@
 package it.polito.mad.lab02.fragments.myadvertisements
 
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
+import androidx.annotation.MenuRes
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.PopupMenu
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import it.polito.mad.lab02.R
+import it.polito.mad.lab02.fragments.listofskills.PublicTimeSlotRecyclerViewAdapter
 import it.polito.mad.lab02.viewmodels.MainActivityViewModel
 import kotlin.system.exitProcess
 
@@ -32,14 +37,14 @@ class TimeSlotsListFragment : Fragment(R.layout.fragment_time_slot_list) {
         vm.setAdvsListenerByCurrentUser()
 
         //Accessing user logged avds
-        vm.loggedUserTimeSlotList.observe(viewLifecycleOwner){timeSlotList ->
+        vm.loggedUserTimeSlotList.observe(viewLifecycleOwner) { timeSlotList ->
             if (recyclerView is RecyclerView) {
                 with(recyclerView) {
                     layoutManager = when {
                         columnCount <= 1 -> LinearLayoutManager(context)
                         else -> GridLayoutManager(context, columnCount)
                     }
-                    adapter = TimeSlotsListRecyclerViewAdapter(timeSlotList.toMutableList()){
+                    adapter = TimeSlotsListRecyclerViewAdapter(timeSlotList.toMutableList()) {
                         vm.deleteTimeSlot(it)
                     }
                 }
@@ -47,8 +52,27 @@ class TimeSlotsListFragment : Fragment(R.layout.fragment_time_slot_list) {
 
             val fab = view.findViewById<FloatingActionButton>(R.id.fab2)
             fab.setOnClickListener { view ->
-                view.findNavController()
-                    .navigate(R.id.action_nav_advertisement_to_timeSlotEditFragment)
+                if (vm.profile.value!!.skills.isNotEmpty()) {
+                    view.findNavController()
+                        .navigate(R.id.action_nav_advertisement_to_timeSlotEditFragment)
+                } else {
+                    val builder: AlertDialog.Builder =
+                        androidx.appcompat.app.AlertDialog.Builder(requireContext())
+
+                    builder.setCancelable(true)
+                    builder.setTitle("Wait a minute!")
+                    builder.setMessage("You can not create a new timeslot without define at least one skill in your profile. Click on the button below to add it!")
+
+                    builder.setNegativeButton("Cancel",
+                        DialogInterface.OnClickListener { dialogInterface, i -> dialogInterface.cancel() })
+
+                    builder.setPositiveButton("ADD SKILL",
+                        DialogInterface.OnClickListener { dialogInterface, i ->
+                            view.findNavController()
+                                .navigate(R.id.action_nav_advertisement_to_editProfileFragment)
+                        })
+                    builder.show()
+                }
             }
 
             val textView = view.findViewById<TextView>(R.id.text_advertisements)
@@ -61,13 +85,23 @@ class TimeSlotsListFragment : Fragment(R.layout.fragment_time_slot_list) {
             }
         }
 
-        val callback = object : OnBackPressedCallback(true){
+        val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                if(!view.findNavController().navigateUp()){
+                if (!view.findNavController().navigateUp()) {
                     exitProcess(1)
                 }
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(callback)
     }
+
+    private fun showSkillConstraintMenu(
+        v: View,
+        @MenuRes menuRes: Int,
+        adapter: PublicTimeSlotRecyclerViewAdapter
+    ) {
+        val popup = PopupMenu(requireContext(), v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+    }
 }
+
