49a073a7662e4a572da37fc8e6c3bd851151725a
lordef <lorenzo.defilippis02@gmail.com>
2022-05-24 12:44:41 +0200

    optimized imports

--- app/src/main/java/it/polito/mad/lab02/fragments/profile/SkillRecyclerViewAdapter.kt
+++ app/src/main/java/it/polito/mad/lab02/fragments/profile/SkillRecyclerViewAdapter.kt
@@ -1,21 +1,14 @@
 package it.polito.mad.lab02.fragments.profile
 
-import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import it.polito.mad.lab02.databinding.FragmentSkillsBinding
-import it.polito.mad.lab02.models.TimeSlot
-import it.polito.mad.lab02.viewmodels.ShowProfileViewModel
 
 
 class SkillRecyclerViewAdapter(
