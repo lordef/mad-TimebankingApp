efa5643de6231d8a097350cd369f63c012649a8f
Andrea Palomba <s291692@studenti.polito.it>
2022-05-26 19:56:59 +0200

    some changes

--- app/src/main/java/it/polito/mad/lab02/fragments/listofskills/RateSomeoneFragment.kt
+++ app/src/main/java/it/polito/mad/lab02/fragments/listofskills/RateSomeoneFragment.kt
@@ -5,56 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
 import it.polito.mad.lab02.R
+import it.polito.mad.lab02.viewmodels.MainActivityViewModel
+import kotlin.math.roundToInt
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RateSomeoneFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RateSomeoneFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val vm by activityViewModels<MainActivityViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val user = view.findViewById<TextView>(R.id.user)
+        val timeslot = view.findViewById<TextView>(R.id.timeslotTitle)
+        val ratingBar = view.findViewById<RatingBar>(R.id.ratingbar)
+        val comment = view.findViewById<EditText>(R.id.comment)
+        val button = view.findViewById<Button>(R.id.button3)
+
+        var stars = 0
+
+        ratingBar.setOnRatingBarChangeListener { ratingBar, fl, b ->
+            stars = fl.roundToInt()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rate_someone, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RateSomeoneFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RateSomeoneFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
