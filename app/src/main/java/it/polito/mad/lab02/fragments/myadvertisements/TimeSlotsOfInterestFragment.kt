c7c2211fdcb3ed365a6ca66a1d60b06355b9c573
Ferdinando Micco <s290247@studenti.polito.it>
2022-05-28 16:37:13 +0200

    screen 1 implemented (Recycle view adapter incomplete)

---
+++ app/src/main/java/it/polito/mad/lab02/fragments/myadvertisements/TimeSlotsOfInterestFragment.kt
@@ -0,0 +1,67 @@
+package it.polito.mad.lab02.fragments.myadvertisements
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.activityViewModels
+import it.polito.mad.lab02.R
+import it.polito.mad.lab02.fragments.communication.MyChatRecyclerViewAdapter
+import it.polito.mad.lab02.fragments.myadvertisements.placeholder.PlaceholderContent
+import it.polito.mad.lab02.fragments.profile.RatingRecyclerViewAdapter
+import it.polito.mad.lab02.models.TimeSlot
+import it.polito.mad.lab02.viewmodels.MainActivityViewModel
+
+/**
+ * A fragment representing a list of Items.
+ */
+class TimeSlotsOfInterestFragment : Fragment(R.layout.fragment_time_slots_of_interest) {
+
+    private val vm by activityViewModels<MainActivityViewModel>()
+
+    private var columnCount = 1
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.list)
+
+        vm.setChatsListener()
+
+        val textView = view.findViewById<TextView>(R.id.text_pub_advertisements)
+
+        vm.requesterChatList.observe(viewLifecycleOwner) { chatList ->
+
+            (activity as AppCompatActivity?)?.supportActionBar?.title = "Chats as requester"
+            if (chatList.isEmpty()) {
+                recyclerView.visibility = View.GONE
+                textView.visibility = View.VISIBLE
+            } else {
+                textView.visibility = View.GONE
+                recyclerView.visibility = View.VISIBLE
+            }
+
+            if (recyclerView is RecyclerView) {
+                with(recyclerView) {
+                    layoutManager = when {
+                        columnCount <= 1 -> LinearLayoutManager(context)
+                        else -> GridLayoutManager(context, columnCount)
+                    }
+                    adapter = TimeSlotsOfInterestRecyclerViewAdapter(chatList.map { it.timeSlot })
+                }
+
+            }
+        }
+
+
+
+    }
+
+}
---
+++ app/src/main/java/it/polito/mad/lab02/fragments/myadvertisements/TimeSlotsOfInterestRecyclerViewAdapter.kt
@@ -0,0 +1,92 @@
+package it.polito.mad.lab02.fragments.myadvertisements
+
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.gson.Gson
+import it.polito.mad.lab02.R
+import it.polito.mad.lab02.databinding.FragmentTimeSlotOfInterestBinding
+import it.polito.mad.lab02.models.TimeSlot
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class TimeSlotsOfInterestRecyclerViewAdapter(
+    private val values: List<TimeSlot>
+) : RecyclerView.Adapter<TimeSlotsOfInterestRecyclerViewAdapter.ViewHolder>() {
+
+    private var displayData = values.toMutableList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            FragmentTimeSlotOfInterestBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(
+            displayData[position],
+            {
+                val bundle = Bundle()
+                bundle.putString("id", displayData[position].id)
+                it.findNavController()
+                    .navigate(
+                        R.id.action_chatFragment_to_nav_single_message,
+                        bundle
+                    )
+            }
+        ) {
+            val bundle = Bundle()
+//            if (displayData[position].publisher.uid == FirebaseAuth.getInstance().currentUser?.uid ?: false) {
+//                bundle.putString("user", Gson().toJson(displayData[position].requester))
+//            } else {
+//                bundle.putString("user", Gson().toJson(displayData[position].publisher))
+//            }
+
+            it.findNavController()
+                .navigate(
+                    R.id.action_nav_chats_to_publicShowProfileFragment,
+                    bundle
+                )
+        }
+    }
+
+    override fun getItemCount(): Int = displayData.size
+
+    inner class ViewHolder(binding: FragmentTimeSlotOfInterestBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val cardTitle: TextView = binding.cardTitle
+        val cardLocation: TextView = binding.cardLocation
+        val cardDate: TextView = binding.cardDate
+        val cardDuration: TextView = binding.cardDuration
+        val card: CardView = binding.cardAdvertisement
+        val cardProfile: TextView = binding.profilePublicAdv
+        val publisher: TextView = binding.profilePublicAdv
+
+        fun bind(timeSlot: TimeSlot, action1: (v: View) -> Unit, action2: (v: View) -> Unit) {
+            card.setOnClickListener(action1)
+            publisher.setOnClickListener(action2)
+            cardTitle.text = timeSlot.title
+            cardLocation.text = timeSlot.location
+            cardDate.text = timeSlot.dateTime
+            cardDuration.text = timeSlot.duration
+            cardProfile.text = timeSlot.userProfile.nickname
+        }
+
+        fun unbind() {
+            card.setOnClickListener(null)
+        }
+    }
+
+}
