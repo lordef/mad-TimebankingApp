e208c11b992ae7579f765dc0796e6414ef86a92d
Andrea Palomba <s291692@studenti.polito.it>
2022-05-30 13:28:39 +0200

    some changes

--- app/src/main/java/it/polito/mad/lab02/fragments/listofskills/RateSomeoneFragment.kt
+++ app/src/main/java/it/polito/mad/lab02/fragments/listofskills/RateSomeoneFragment.kt
@@ -42,9 +42,9 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
 
         val rated = view.findViewById<TextView>(R.id.userRated)
         val ratedImageView = view.findViewById<ImageView>(R.id.imageView3)
-        val ratingBar = view.findViewById<RatingBar>(R.id.ratingbar)
-        val comment = view.findViewById<EditText>(R.id.commentEditText)
-        val button = view.findViewById<Button>(R.id.button3)
+//        val ratingBar = view.findViewById<RatingBar>(R.id.ratingbar)
+//        val comment = view.findViewById<EditText>(R.id.commentEditText)
+//        val button = view.findViewById<Button>(R.id.button3)
 
         val yourReview = view.findViewById<ConstraintLayout>(R.id.constraintLayoutFirstReview)
         val theirReview = view.findViewById<ConstraintLayout>(R.id.constraintLayoutSecondReview)
@@ -180,7 +180,7 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
 
         var stars = 0
 
-        ratingBar.setOnRatingBarChangeListener { ratingBar, fl, b ->
+        ratingBar.setOnRatingBarChangeListener { _, fl, _ ->
             stars = fl.roundToInt()
         }
 
