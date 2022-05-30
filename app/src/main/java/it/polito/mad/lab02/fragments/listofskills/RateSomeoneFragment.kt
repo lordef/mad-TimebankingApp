2a13716b9ee64b98e484044420a1e55b11bb88d4
Andrea Palomba <s291692@studenti.polito.it>
2022-05-30 13:58:43 +0200

    some changes in layout

--- app/src/main/java/it/polito/mad/lab02/fragments/listofskills/RateSomeoneFragment.kt
+++ app/src/main/java/it/polito/mad/lab02/fragments/listofskills/RateSomeoneFragment.kt
@@ -144,7 +144,7 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // TODO: chiedere a Fabiano
+        // TODO: chiedere a Fabiano x il listener quando si torna indietro cn la freccia in alto
         return when (item.itemId) {
             android.R.id.home -> {
                 findNavController().navigateUp()
@@ -204,6 +204,7 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
         val ratingBarFR = view?.findViewById<RatingBar>(R.id.ratingBarFirstReview)!!
         val dateFR = view?.findViewById<TextView>(R.id.textViewDateFirstReview)!!
         val commentFR = view?.findViewById<TextView>(R.id.textViewCommentFirstReview)!!
+        val otherNameFR = view?.findViewById<TextView>(R.id.textViewReviewGivenToUser)!!
 
         // second review
         val nameTitleSR = view?.findViewById<TextView>(R.id.textViewUsername)!!
@@ -220,6 +221,7 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
             ratingBarFR.rating = ratings[0].starsNum.toFloat()
             dateFR.text = ratings[0].timestamp
             commentFR.text = ratings[0].comment
+            otherNameFR.text = ratings[0].rated.nickname
 
             nameTitleSR.text = ratings[1].rater.nickname
             imageSR.load(Uri.parse(ratings[1].rater.imageUri))
@@ -235,6 +237,7 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
             ratingBarFR.rating = ratings[1].starsNum.toFloat()
             dateFR.text = ratings[1].timestamp
             commentFR.text = ratings[1].comment
+            otherNameFR.text = ratings[1].rated.nickname
 
             nameTitleSR.text = ratings[0].rater.nickname
             imageSR.load(Uri.parse(ratings[0].rater.imageUri))
@@ -246,7 +249,7 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
         }
     }
 
-    fun oneRatingPresent(rating: Rating, yourProfile: Boolean){
+    private fun oneRatingPresent(rating: Rating, yourProfile: Boolean){
 
         if(yourProfile){  // first review
 
@@ -255,12 +258,14 @@ class RateSomeoneFragment : Fragment(R.layout.fragment_rate_someone) {
             val ratingBarFR = view?.findViewById<RatingBar>(R.id.ratingBarFirstReview)!!
             val dateFR = view?.findViewById<TextView>(R.id.textViewDateFirstReview)!!
             val commentFR = view?.findViewById<TextView>(R.id.textViewCommentFirstReview)!!
+            val otherNameFR = view?.findViewById<TextView>(R.id.textViewReviewGivenToUser)!!
 
             imageFR.load(Uri.parse(rating.rater.imageUri))
             nameFR.text = rating.rater.nickname
             ratingBarFR.rating = rating.starsNum.toFloat()
             dateFR.text = rating.timestamp
             commentFR.text = rating.comment
+            otherNameFR.text = rating.rated.nickname
 
         }else{  // second review
 
